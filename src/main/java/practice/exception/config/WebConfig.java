ccbc504cd9cb8f2139212360ac83a4130aec330a
YHCHOI <cyh6809@gmail.com>
2023-04-26 13:52:13 +0900

    ExceptionResolver 실습

--- src/main/java/practice/exception/config/WebConfig.java
+++ src/main/java/practice/exception/config/WebConfig.java
@@ -3,6 +3,7 @@ package practice.exception.config;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import practice.exception.filter.LogFilter;
@@ -10,6 +11,10 @@ import practice.exception.interceptor.LogInterceptor;
 
 import jakarta.servlet.DispatcherType;
 import jakarta.servlet.Filter;
+import practice.exception.resolver.MyHandlerExceptionResolver;
+import practice.exception.resolver.UserHandlerExceptionResolver;
+
+import java.util.List;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -31,5 +36,11 @@ public class WebConfig implements WebMvcConfigurer {
         filterRegistrationBean.setDispatcherTypes(DispatcherType.REQUEST, DispatcherType.ERROR);
 
         return filterRegistrationBean;
+    }
+
+    @Override
+    public void extendHandlerExceptionResolvers(List<HandlerExceptionResolver> resolvers) {
+        resolvers.add(new MyHandlerExceptionResolver());
+        resolvers.add(new UserHandlerExceptionResolver());
     }
 }
